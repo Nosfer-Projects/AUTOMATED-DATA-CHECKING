218baaa9155fa055a564e31145d401289166fa5a
Nosfer-Projects <114942472+Nosfer-Projects@users.noreply.github.com>
2022-12-19 15:38:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
 import time
 
 chrome_driver_path = Service("[Path to your driver]")
